33a0e60129c020b8021e75352cd116526ab836f2
Gandharv Kulkarni <gandharvakulkarni2003@gmail.com>
2024-04-23 23:43:47 +0530

    Minor Change in UI

--- src/Components/Customer/Banner.js
+++ src/Components/Customer/Banner.js
@@ -9,10 +9,15 @@ const Banner = () => {
   const role = localStorage.getItem("role")
   return (
     <Box minH="100vh" className='bg-hero text-light d-flex align-items-center flex-column justify-content-center animate__fadeIn animate__animated'>
-      <Heading className='fw-bolder fs-1 fst-italic py-1'>Welcome</Heading>
-      <Heading className='mt-3 mb-4 fw-normal fs-1 py-1 text-center lh-lg'>Effortless meal planning for a healthier you <br /> with our app! <Text as="span"><MdFastfood className='fs-2 d-inline text-warning fa-fade' /></Text> </Heading>
-
-      <Link as={ReactLink} to={`${isLoggedIn ? (role==="owner" ? '/dashboard' : "/customer") : '/login'}`}>
+      <Heading className=''>Welcome to</Heading>
+      <Heading color="orange.500" className="fw-bolder fs-1 fst-italic">Anna<Text as="span" color="orange.400">Prasadam</Text>
+      <Text as="span"><MdFastfood className='fs-2 d-inline text-warning fa-fade ms-3' /></Text>
+      </Heading>
+      <Heading className='mt-3 mb-4 fw-normal fs-1 py-1 text-center lh-lg'>
+        Your Menu is One Click Away...
+        
+      </Heading>
+      <Link as={ReactLink} to={`${isLoggedIn ? (role === "owner" ? '/dashboard' : "/customer") : '/login'}`}>
         <Button colorScheme='orange'>Get Started</Button>
       </Link>
     </Box>
